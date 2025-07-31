02c9d40912a42f8ca51d6898bb065fe5ae79325c
Nuwah12 <nburget12@gmail.com>
2025-07-31 15:22:11 -0400

    wrapper scripts for operations

---
+++ scripts/align_contigs_to_ref.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Align, convert, and sort assembled contigs to refrence genome
+
+
+USAGE="Usage: $0 -f <reference fasta> -a <assembly fasta> -o <output prefix>"
+
+function print_usage_exit()
+{
+	if [[ ! -z $1 ]]; then
+		echo -e "Unknown parameter '$1' \n${USAGE}"
+		exit 1
+	fi
+	echo ${USAGE}
+	exit 1
+}
+
+if [[ $# -eq 0 ]]; then
+	echo $USAGE
+	exit 1
+fi
+
+ref=""
+assem=""
+threads=48
+out_prefix=""
+
+while [[ $# -gt 0 ]]; do
+	case "$1" in
+		-f | --fasta )
+			ref="$2"
+			shift 2 # Shift past both argument and value
+			;;
+		-o | --output_prefix )
+			out_prefix="$2"
+			shift 2
+			;;
+		-a | --assembly_fasta )
+			assem="$2"
+			shift 2
+			;;
+		-* | --* )
+			print_usage_exit $1
+			;;
+	esac
+done
+
+cmd="minimap2 -ax asm5 -Y -t ${threads} ${ref} ${assem} | samtools view -bS - | samtools sort -@ ${threads} -o ${out_prefix}.sorted.bam"
+echo ${cmd} > "${out_prefix}.cmd"
+
+eval ${cmd}
+
---
+++ scripts/hifiasm_assembly.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Assemble ONT reads with HiFiasm
+
+USAGE="Usage: $0 -f <fastq>"
+
+function print_usage_exit()
+{
+	if [[ ! -z $1 ]]; then
+		echo -e "Unknown parameter '$1' \n${USAGE}"
+		exit 1
+	fi
+	echo ${USAGE}
+	exit 1
+}
+
+if [[ $# -eq 0 ]]; then
+	echo $USAGE
+	exit 1
+fi
+
+threads=48
+fastq=""
+out_prefix=""
+
+while [[ $# -gt 0 ]]; do
+	case "$1" in
+		-f | --fastq )
+			fastq="$2"
+			shift 2 # Shift past both argument and value
+			;;
+		-o | --output_prefix )
+			out_prefix="$2"
+			shift 2
+			;;
+		-* | --* )
+			print_usage_exit $1
+			;;
+	esac
+done
+
+cmd="hifiasm -o ${out_prefix}.asm --ont -t ${threads} ${fastq}"
+echo ${cmd} > "${out_prefix}.hifiasm.cmd"
+
+eval ${cmd}
+
