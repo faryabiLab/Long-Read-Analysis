2cf65117cf48be142c8186135f5db3907db6b5e2
Noah Burget <51685498+burgeng@users.noreply.github.com>
2025-09-30 21:47:59 -0400

    Update align_contigs_to_ref.sh

--- scripts/align_contigs_to_ref.sh
+++ scripts/align_contigs_to_ref.sh
@@ -74,6 +74,8 @@ cat > "$script_name" <<EOF
 # Run by: $(whoami)
 # Environment: $(basename "$CONDA_PREFIX")
 # Run in directory $(pwd)
+# Date: $(date +"%m/%d/%Y")
+# Time: $(date +"%I:%M %p")
 # ========================================
 
 minimap2 -ax asm5 -Y -t ${threads} ${ref_full} ${assem_full} | samtools view -bS - | samtools sort -@ ${threads} -o ${out_prefix}.sorted.bam
